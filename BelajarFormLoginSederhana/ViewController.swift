fb31b9078de355bd4ace404adead854097f5469f
Nova Arisma <novaarisma95@gmail.com>
2020-09-06 16:55:29 +0700

    Initial Commit

---
+++ BelajarFormLoginSederhana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarFormLoginSederhana
+//
+//  Created by Nova Arisma on 9/6/20.
+//  Copyright © 2020 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
